3195bbf5e8a7bc0873af95c7b951b98f048ab15a
Digitalfox <webartisan.digitalfox@gmail.com>
2023-08-18 12:34:07 +0530

    Project Init Readme updated

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #Templating Project
 
 ##Here we can learn templating
+
+#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
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
     echo 'Sample File';
+
+    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
 ?>
